804527b6923a3abcf860da66faf15f61a2987143
Liu JiaJun <jiajunliupersonal@gmail.com>
2024-05-01 20:08:16 +0800

    Revert "Update BuildArrayfromPermutation.cpp"
    This reverts commit c879497fb6cfc93871df82657e7bb9027cb07995.

--- SequentialList/BuildArrayfromPermutation.cpp
+++ SequentialList/BuildArrayfromPermutation.cpp
@@ -33,13 +33,11 @@ Constraints:
 
 class Solution {
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
+    vector<int> buildArray(vector<int>& nums) {
         vector<int> ans;
-        for(int i = 0; i < nums.size(); i++){
-            ans.push_back(nums[i]);
-        }
-        for(int i = 0; i < nums.size(); i++){
-            ans.push_back(nums[i]);
+        for(int i=0; i<nums.size(); i++){
+            int ansElement = nums[nums[i]];
+            ans.push_back(ansElement);
         }
         return ans;
     }
