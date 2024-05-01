c879497fb6cfc93871df82657e7bb9027cb07995
Liu JiaJun <jiajunliupersonal@gmail.com>
2024-05-01 20:06:06 +0800

    Update BuildArrayfromPermutation.cpp

--- SequentialList/BuildArrayfromPermutation.cpp
+++ SequentialList/BuildArrayfromPermutation.cpp
@@ -33,11 +33,13 @@ Constraints:
 
 class Solution {
 public:
-    vector<int> buildArray(vector<int>& nums) {
+    vector<int> getConcatenation(vector<int>& nums) {
         vector<int> ans;
-        for(int i=0; i<nums.size(); i++){
-            int ansElement = nums[nums[i]];
-            ans.push_back(ansElement);
+        for(int i = 0; i < nums.size(); i++){
+            ans.push_back(nums[i]);
+        }
+        for(int i = 0; i < nums.size(); i++){
+            ans.push_back(nums[i]);
         }
         return ans;
     }
